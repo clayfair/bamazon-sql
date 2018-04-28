3450ba6811ba3112c81da20898407a4fb534b376
Clay Fairbanks <clayafairbanks@gmail.com>
2018-04-28 11:34:04 -0400

    bamaozn

---
+++ bamazon.sql
@@ -0,0 +1,48 @@
+-- Drops the favorite_db if it exists currently --
+DROP DATABASE IF EXISTS bamazon;
+-- Creates the "favorite_db" database --
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+   id INTEGER NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100),
+  department_name VARCHAR(100),
+  price DECIMAL(10,2),
+  stock_quantity DECIMAL(10,2),
+  PRIMARY KEY (id)
+);
+
+
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Bubly Sparkling Water", "Food & Grocery", 7.16, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Lip Trip SPF 15", "Beauty & Health", 39.99, 123);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Harry Potter and the Sorcerer's Stone", "Books & Audible", 6.99, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Jumanji: Welcome To The Jungle", "Movies, Music & Games", 4.99, 120);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Ceramic Cup", "Handmade", 27.99, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Under Armour Men's Tech Short Sleeve T-Shirt", "Sports & Outdoors", 24.00, 204);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Champion Men's Jersey Short", "Sports & Outdoors", 13.99, 400);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("To Kill a Mockingbird", "Books & Audible", 8.99, 783);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Star Wars: The Last Jedi (Theatrical Version)", "Movies, Music & Games", 4.99, 300);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Glass Cleaner", "Automotive & Industrial", 15.29, 38);
---
+++ bamazonCustomer.js
@@ -0,0 +1,89 @@
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var id = parseInt(id);
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "3lilNinjas",
+  database: "bamazon"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  start();
+});
+
+function start() {
+  //display all of the items available for sale. Include the ids, names, and prices of products for sale.
+  connection.query('SELECT * FROM products', function (error, results) {
+    if (error) throw error;
+    results.forEach(function (RowDataPacket) {
+      console.log("ID: " + RowDataPacket.id);
+      console.log("Product Name: " + RowDataPacket.product_name);
+      console.log("Price: " + RowDataPacket.price);
+      // console.log("Deapartment Name: " + RowDataPacket.department_name);
+      // console.log("Quantity: " + RowDataPacket.stock_quantity);
+      console.log("-------------------------------------");
+    });
+    prompt();
+  });
+}
+//^^^^^^^^^^^^^I'm going to clean this to use console.table
+
+
+//prompt users with two messages: 
+//1. ask them the ID of the product they would like to buy
+function prompt() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "input",
+      message: [
+        "What is the ID # of the product you would like to buy?"
+      ]
+    })
+    .then(function (answer) {
+      id = answer.action;
+      //check if the product is available
+      checkAvailability();
+      // console.log(id);
+    });
+}
+
+//check if your store has enough of the product to meet the customer's request
+function checkAvailability() {
+  //display all of the items available for sale. Include the ids, names, and prices of products for sale.
+  connection.query('SELECT * FROM products', function (error, results) {
+    if (error) throw error;
+    results.forEach(function (RowDataPacket) {
+
+    });
+    Quantity();
+  });
+}
+
+
+//2. ask how many units of the product they would like to buy. 
+function Quantity() {
+  var id = parseInt(id);
+  inquirer
+    .prompt({
+      name: "action",
+      type: "input",
+      message: [
+        "How many would you like to buy?"
+      ]
+    })
+    .then(function (answer) {
+      
+    });
+}
+
+//IN PROGRESSSSSSSSS: ONSITE INTERVIEW THIS WEEK THEN FINISHING THIS BAD BOY BY MONDAY 5/1
