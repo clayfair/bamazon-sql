a02092d434532309a352e3f2839752fdff7c094f
Clay Fairbanks <clayafairbanks@gmail.com>
2018-05-14 18:09:17 -0400

    no password

--- bamazon.sql
+++ bamazon.sql
@@ -14,7 +14,6 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
-
 SELECT * FROM products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "3lilNinjas",
+  password: "",
   database: "bamazon"
 });
 
